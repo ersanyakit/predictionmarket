4cc9221ff900684637f43f0d85690e1bfa21af08
Ersan YAKIT <ersanyakit@yahoo.com.tr>
2024-09-27 21:14:22 +0700

    fixes

--- ui/src/components/market/create/index.tsx
+++ ui/src/components/market/create/index.tsx
@@ -1,5 +1,24 @@
+import { Input } from "@nextui-org/react";
 import { FC } from "react";
 
 
 export const CreateMarket: FC<any> = ({ color, className, ...rest }) => {
+
+    return(<>
+        <div className="w-full flex flex-col gap-2">
+            <div className="w-full">
+
+            <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+                <Input type="text" label="Title" />
+                <Input type="text" label="Description" />
+                <Input type="text" label="LogoURI" />
+
+            </div>
+            </div>
+            <div className="w-full">
+
+            </div>
+        </div>
+
+    </>)
 }
--- ui/src/contracts/addresses/index.tsx
+++ ui/src/contracts/addresses/index.tsx
@@ -1,5 +1,39 @@
 
 export const CONTRACT_ADRESSES: any = {
   ARENADIAMOND: "0xB97c8C8C10938a9fC13d64E2722b86A5dF2E1E5b",
-  FAN_TOKENS:[]
+  FAN_TOKENS:[
+    {
+      "chainId": 88882,
+      "address": "0x945EeD98f5CBada87346028aD0BeE0eA66849A0e",
+      "name": "Juventus",
+      "symbol": "JUV",
+      "decimals": 0,
+      "logoURI": "https://raw.githubusercontent.com/kewlexchange/assets/main/chiliz/tokens/0x454038003a93cf44766af352f74bad6b745616d0/logo.svg"
+  },
+  {
+      "chainId": 88882,
+      "address": "0x641d040dB51398Ba3a4f2d7839532264EcdCc3aE",
+      "name": "AC Milan",
+      "symbol": "ACM",
+      "decimals": 0,
+      "logoURI": "https://raw.githubusercontent.com/kewlexchange/assets/main/chiliz/tokens/0xf9c0f80a6c67b1b39bddf00ecd57f2533ef5b688/logo.svg"
+  },
+  {
+      "chainId": 88882,
+      "address": "0x9B9C9AAa74678FcF4E1c76eEB1fa969A8E7254f8",
+      "name": "Tottenham Hotspur",
+      "symbol": "SPURS",
+      "decimals": 0,
+      "logoURI": "https://raw.githubusercontent.com/kewlexchange/assets/main/chiliz/tokens/0x93d84ff2c5f5a5a3d7291b11af97679e75eeac92/logo.svg"
+  },
+  {
+      "chainId": 88882,
+      "address": "0x7F73C50748560BD2B286a4c7bF6a805cFb6f735d",
+      "name": "FC Barcelona",
+      "symbol": "BAR",
+      "decimals": 0,
+      "logoURI": "https://raw.githubusercontent.com/kewlexchange/assets/main/chiliz/tokens/0xfd3c73b3b09d418841dd6aff341b2d6e3aba433b/logo.svg"
+  },
+
+  ]
 };
